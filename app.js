ab2f9cb51596bcd6f91f5cce80cc06e2eb74e509
isabellesavannah <isabellet.208@gmail.com>
2021-02-17 19:04:42 -0700

    complete

---
+++ app.js
@@ -0,0 +1,390 @@
+class Ardvark{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let ardy = new Ardvark("Ardy", "tan", 2, 50)
+class Baboon{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let babby = new Baboon("Babby", "brown", 3, 100)
+class Cuddlefish{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let cuddles = new Cuddlefish("Cuddles","yellow",1,1)
+class Dodo{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let doodoo = new Dodo("Doodoo", "white", 2, 30)
+class Elephant{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let elly = new Elephant("Elly", "grey", 7, 2000)
+class Frog{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let hoppy = new Frog("Hoppy", "green", 1, 1)
+class Giraffe{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let smalls = new Giraffe("Smalls", "yellow", 15, 1000)
+class Hare{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let harold = new Hare("Harold", "white", 1, 20)
+class Impala{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let rims = new Impala("Rims", "tan", 3, 100)
+class Jackal{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let jackie = new Jackal("Jackie", "tan", 3, 120)
+class Kangaroo{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let Jack = new Kangaroo("Jack", "tan", 5, 200)
+class Lizard{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let lizzie = new Lizard("Lizzie", "green", 1, 5)
+class Monkey{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let georgie = new Monkey("Georgie", "brown", 3, 70)
+class NurseShark{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let ratched = new NurseShark("Ratched", "grey", 3, 70)
+class Octopus{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let octy = new Octopus("Octy", "purple", 2, 55)
+class Panda{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let po = new Panda("Po", "black and white", 4, 300)
+class Quail{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let quin = new Quail ("Quin", "speckled", 1, 3)
+class Rat{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let splinter = new Rat("Splinter", "brown", 2, 7)
+class Snake{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let kaa = new Snake ("Kaa", "green", 7, 50)
+class Trex{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let rex = new Trex("Rex", "green", 12, 3000)
+class Urchin{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let fluffy = new Urchin("Fluffy", "purple", 1, 1)
+class Vulture{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let chad = new Vulture("Chad", "white", 2, 50)
+class Walrus{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let walter = new Walrus ("Walter", "brown", 5, 800)
+class XrayFish{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let vision = new XrayFish("Vision", "clear", 1, 1)
+class Yak{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let rally = new Yak("Rally", "black", 6, 500)
+class ZebraShark{
+    constructor(
+        name = '',
+        color = '',
+        height = 0,
+        weight = 0,
+        
+    ) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+let za = new ZebraShark("Za", "brindle", 5, 100)
